2051d40bd54bbd6d944ca71a4d5d80e3c178324b
Antti Kaihola <antti.kaihola@eniram.fi>
2013-09-06 15:51:40 +0300

    Added triggered dependents tracking with caching. Added some tests.

--- slacker/base.py
+++ slacker/base.py
@@ -4,48 +4,147 @@ import logging
 log = logging.getLogger('slacker.base')
 
 
+_triggered_cache = {}
+
+
 class DIRTY:
     pass
 
 
 class Node(object):
-    def __init__(self, name):
+    def __init__(self, name, triggered=False):
+        """Initialize a node
+
+        Arguments:
+          name: the internal name of the node; used in the repr
+          triggered: should this node be automatically evaluated when any of
+                     its depended nodes changes value
+
+        """
         self.name = name
         self._value = DIRTY
         self._dependents = set()
+        self.triggered = triggered
 
-    def _evaluate(self):
-        raise NotImplementedError('You must implement the _evaluate() method '
-                                  'in subclasses of Node.')
+    def connect(self, dependent):
+        """Set the given node as a dependent of this node
+
+        Immediately paints the new dependent dirty if this node has already
+        been evaluated.
+
+        Connecting nodes always invalidates the triggered nodes cache.
+
+        """
+        if dependent not in self._dependents:
+            self._dependents.add(dependent)
+            if self._value is not DIRTY:
+                dependent.set_value(DIRTY, make_cache=False)
+        _triggered_cache.clear()
 
     def get_value(self):
+        """Return node value, evaluate if needed and paint dependents dirty"""
         if self._value is DIRTY:
             self._value = self._evaluate()
             log.debug('EVALUATED %s: %s', self.name, self._value)
             self._set_dependents_dirty()
         return self._value
 
-    def set_value(self, value):
-        if value != self._value:
-            self._value = value
-            self._set_dependents_dirty()
+    def set_value(self, value, make_cache=True):
+        """Set a new value for this node
+
+        If this caused the value to change, paints dependent nodes dirty and
+        returns the set of those dependent nodes which are marked "triggered"
+        and should be re-evaluated.
+
+        When called from external code, the ``make_cache`` argument must be
+        ``True`` so the return value is cached.  Internal calls set
+        ``make_cache=False`` so memory isn't wasted for caching the triggered
+        dependents of intermediate nodes.
+
+        """
+        if value == self._value:
+            return set()
+        self._value = value
+        self._set_dependents_dirty()
+        return self.get_triggered_dependents(make_cache=make_cache)
+
+    def _evaluate(self):
+        raise NotImplementedError('You must implement the _evaluate() method '
+                                  'in subclasses of Node.')
+
+    def get_triggered_dependents(self, make_cache=True):
+        """Return the set of triggered dependent nodes
+
+        The set includes nodes which are marked as triggered and are included
+        in the dependent chain from this node.
+
+        The result is cached for the node if ``make_cache == True``, but
+        caching is suppressed for recursively walked dependent nodes.  This way
+        we only use cache memory only for nodes whose triggered dependents are
+        queried from external code.
+
+        """
+        if self in _triggered_cache:
+            return _triggered_cache[self]
+        triggered = set()
+        for dependent in self._dependents:
+            if dependent.triggered:
+                triggered.add(dependent)
+            triggered |= dependent.get_triggered_dependents(make_cache=False)
+        if make_cache:
+            _triggered_cache[self] = triggered
+        return triggered
 
     def _set_dependents_dirty(self):
-        for dependent in self._dependents:
-            dependent.set_value(DIRTY)
+        """Paint all dependents dirty
 
-    def connect(self, dependent):
-        if dependent not in self._dependents:
-            self._dependents.add(dependent)
-            if self._value is not DIRTY:
-                dependent.set_value(DIRTY)
+        Paints direct dependents dirty, which causes recursive painting for the
+        whole dependents tree.
+
+        """
+        for dependent in self._dependents:
+            dependent.set_value(DIRTY, make_cache=False)
 
     def __unicode__(self):
         return unicode(self.get_value())
 
     def __repr__(self):
-        return ('<{self.__class__.__name__} {self._value}>'
+        return ('<{self.__class__.__name__} {self.name}: {self._value}>'
                 .format(self=self,
                         value=unicode(self).encode('ascii', errors='replace')))
 
 
+def update_nodes_iter(nodes_and_values):
+    """Update values of multiple nodes and trigger dependents
+
+    This is a generator which iterates through the set of triggered dependent
+    nodes.
+
+    """
+    triggered = set()
+    for node, new_value in nodes_and_values:
+        triggered |= node.set_value(new_value)
+    for node in triggered:
+        node.get_value()  # trigger evaluation
+        yield node
+
+
+def update_nodes(nodes_and_values):
+    """Update values of multiple nodes and trigger dependents
+
+    Use this variant of the ``update_nodes*`` functions if you don't need to
+    access the set of triggered dependent nodes.
+
+    """
+    for _node in update_nodes_iter(nodes_and_values):
+        pass
+
+
+def update_nodes_get_triggered(nodes_and_values):
+    """Update values of multiple nodes and trigger dependents
+
+    This variant of the ``update_nodes*`` functions returns triggered
+    dependents as a Python set.
+
+    """
+    return set(update_nodes_iter(nodes_and_values))
---
+++ slacker/tests/test_base.py
@@ -0,0 +1,145 @@
+from unittest import TestCase
+from slacker.base import (DIRTY,
+                          Node,
+                          update_nodes_get_triggered,
+                          _triggered_cache)
+
+
+class IncompleteNode(Node):
+    """A Node subclass which doesn't implement ._evaluate()"""
+    pass
+
+
+class ConstantNode(Node):
+    """A Node subclass which always evaluates to the value 1"""
+    def _evaluate(self):
+        return 1
+
+
+class NodeTestCase(TestCase):
+    def test_missing_evaluate(self):
+        """The Node class must be subclassed, it doesn't work by itself"""
+        with self.assertRaises(NotImplementedError):
+            IncompleteNode('name').get_value()
+
+    def test_name(self):
+        """A node uses its name in its string representation"""
+        self.assertEqual('<ConstantNode node: 1>',
+                         repr(ConstantNode('node')))
+
+
+class NodeDependentTestCase(TestCase):
+    def setUp(self):
+        self.root = ConstantNode('root')
+        self.dependent = ConstantNode('dependent', triggered=False)
+        self.root.connect(self.dependent)
+        self.triggered = ConstantNode('triggered', triggered=True)
+        self.root.connect(self.triggered)
+
+    def test_keep_dirty(self):
+        """Setting a dirty Node as dirty doesn't trigger dependents"""
+        triggered_nodes = self.root.set_value(DIRTY)
+        self.assertEqual(set(), triggered_nodes)
+
+    def test_set_value_triggers_dependents(self):
+        """Setting a value to a dirty Node triggers dependents"""
+        triggered_nodes = self.root.set_value(0)
+        self.assertEqual({self.triggered}, triggered_nodes)
+
+    def test_get_triggered_dependents(self):
+        """Setting a value to a dirty Node triggers dependents"""
+        triggered_nodes = self.root.get_triggered_dependents()
+        self.assertEqual({self.triggered}, triggered_nodes)
+
+    def test_get_deep_triggered_dependents(self):
+        """Setting a value to a dirty Node triggers dependents tree"""
+        child1 = ConstantNode('child1', triggered=True)
+        child2 = ConstantNode('child2', triggered=True)
+        self.triggered.connect(child1)
+        self.triggered.connect(child2)
+        triggered_nodes = self.root.get_triggered_dependents()
+        self.assertEqual({self.triggered, child1, child2}, triggered_nodes)
+
+
+class CountingNode(ConstantNode):
+    def __init__(self, *args, **kwargs):
+        super(CountingNode, self).__init__(*args, **kwargs)
+        self.call_count = 0
+
+    def get_triggered_dependents(self, *args, **kwargs):
+        self.call_count += 1
+        return super(CountingNode, self).get_triggered_dependents(*args,
+                                                                  **kwargs)
+
+
+class TriggeredCacheTestCase(TestCase):
+    def setUp(self):
+        self.root = CountingNode('root')
+        self.branch = CountingNode('branch', triggered=True)
+        self.leaf1 = CountingNode('leaf1', triggered=True)
+        self.leaf2 = CountingNode('leaf2', triggered=True)
+        self.root.connect(self.branch)
+        self.branch.connect(self.leaf1)
+        self.branch.connect(self.leaf2)
+
+    def test_cache_content(self):
+        """Triggered dependents are cached for each node"""
+        self.root.get_triggered_dependents()
+        self.assertEqual({self.root: {self.branch, self.leaf1, self.leaf2}},
+                         _triggered_cache)
+
+    def test_connect_clears_cache(self):
+        """Connecting nodes invalidates the triggered nodes cache"""
+        self.root.get_triggered_dependents()
+        self.assertEqual({self.root: {self.branch, self.leaf1, self.leaf2}},
+                         _triggered_cache)
+        self.root.connect(CountingNode('leaf3'))
+        self.assertEqual({}, _triggered_cache)
+
+    def test_get_triggered_dependents(self):
+        """get_triggered_dependents() isn't called again for cached nodes"""
+        self.root.get_triggered_dependents()
+        self.assertEqual(1, self.root.call_count)
+        self.assertEqual(1, self.branch.call_count)
+        self.assertEqual(1, self.leaf1.call_count)
+        self.assertEqual(1, self.leaf2.call_count)
+        self.root.get_triggered_dependents()
+        self.assertEqual(2, self.root.call_count)
+        self.assertEqual(1, self.branch.call_count)
+        self.assertEqual(1, self.leaf1.call_count)
+        self.assertEqual(1, self.leaf2.call_count)
+
+
+class NodeSetValueTestCase(TestCase):
+    def test_set_value(self):
+        """A value set to a dirty Node is stored in the object"""
+        node = Node('name')
+        node.set_value(0)
+        self.assertEqual(0, node._value)
+
+
+class UpdateNodesTestCase(TestCase):
+    def setUp(self):
+        self.root = CountingNode('root')
+        self.branch1 = CountingNode('branch1', triggered=True)
+        self.branch2 = CountingNode('branch2', triggered=True)
+        self.leaf1 = CountingNode('leaf1', triggered=True)
+        self.leaf2 = CountingNode('leaf2', triggered=True)
+        self.leaf3 = CountingNode('leaf3', triggered=True)
+        self.leaf4 = CountingNode('leaf4', triggered=True)
+        self.root.connect(self.branch1)
+        self.root.connect(self.branch2)
+        self.branch1.connect(self.leaf1)
+        self.branch1.connect(self.leaf2)
+        self.branch2.connect(self.leaf3)
+        self.leaf3.connect(self.leaf4)
+
+    def test_only_leafs_triggered_1(self):
+        """Updating a Node only triggers its descendents"""
+        triggered = update_nodes_get_triggered([(self.branch1, 2)])
+        self.assertEqual({self.leaf1, self.leaf2}, triggered)
+
+    def test_only_leafs_triggered_2(self):
+        """Updating a Node only triggers its descendents"""
+        triggered = update_nodes_get_triggered([(self.branch2, 2)])
+        self.assertEqual({self.leaf3, self.leaf4}, triggered)
