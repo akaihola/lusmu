cfba7a1525d78f0813380045f471f4154713ebd7
Antti Kaihola <antti.kaihola@eniram.fi>
2013-09-09 13:54:47 +0300

    Added a setup.py script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name='lusmu',
+      version='0.0.1',
+      packages=['lusmu'],
+      author='Antti Kaihola',
+      author_email='antti.kaihola@eniram.fi',
+      description='A lazy/forced evaluation library',
+      keywords='eniram reactive lazy evaluation',
+      url='https://github.com/akaihola/lusmu')
